4d35e41e57c457f639c63b7d202859de032ce34c
martin.trummer <martin.trummer@tmtron.com>
2016-09-13 10:27:56 +0000

    added JUnit test to the green-annotations project

---
+++ green-annotations/src/test/java/com/tmtron/greenannotations/EventBusGreenRobotTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2016 Martin Trummer (martin.trummer@tmtron.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tmtron.greenannotations;
+
+import org.androidannotations.internal.AndroidAnnotationProcessor;
+import org.androidannotations.testutils.AAProcessorTestHelper;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EventBusGreenRobotTest extends AAProcessorTestHelper {
+
+    @Before
+    public void setUp() {
+        // register our AndroidManifest.xml
+        addManifestProcessorParameter(EventBusGreenRobotTest.class);
+        addProcessor(AndroidAnnotationProcessor.class);
+        // explicitly set the encoding
+        // some test-files include special characters:
+        // e.g. the copyright symbol
+        addCompilerOptions("-encoding", "UTF-8");
+    }
+
+    @Test
+    public void activitySubclassInManifestCompiles() {
+        assertCompilationSuccessful(compileFiles(
+                SomeActivity.class,
+                SomeEbean.class
+        ));
+    }
+
+    @Test
+    public void eBeanCompiles() {
+        assertCompilationSuccessful(compileFiles(
+                SomeEbean.class
+        ));
+    }
+
+    /**
+     * the compilation of {@link SomeEbeanError} is expected to fail,
+     * because this {@link EventBusGreenRobot} annotation is used
+     * on a variable of type string (instead of {@link org.greenrobot.eventbus.EventBus})
+     */
+    @Test
+    public void eBeanCompilationFails() {
+        assertCompilationError(compileFiles(
+                SomeEbeanError.class
+        ));
+    }
+
+}
---
+++ green-annotations/src/test/java/java/greenannotations/R.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2016 Martin Trummer (martin.trummer@tmtron.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package java.greenannotations;
+
+/**
+ * This dummy R class is needed for the JUnit tests.
+ * <p></p>
+ * If it does not exist, the tests will fail with this error:
+ * <pre>
+ * java.lang.AssertionError: Expected no errors, found error: The generated java.greenannotations.R class cannot be found
+ * </pre>
+ */
+public class R {
+
+}
