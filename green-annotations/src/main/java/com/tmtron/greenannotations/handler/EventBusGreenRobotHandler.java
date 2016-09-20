384f55c3b7dd71187e4117ad41bb2eb950007381
martin.trummer <martin.trummer@tmtron.com>
2016-09-20 16:55:29 +0000

    issue #8 * improved java-docs * configured mvn site

--- green-annotations/src/main/java/com/tmtron/greenannotations/handler/EventBusGreenRobotHandler.java
+++ green-annotations/src/main/java/com/tmtron/greenannotations/handler/EventBusGreenRobotHandler.java
@@ -24,6 +24,7 @@ import org.androidannotations.handler.MethodInjectionHandler;
 import org.androidannotations.helper.InjectHelper;
 import org.androidannotations.holder.EComponentHolder;
 import org.androidannotations.holder.HasLifecycleMethods;
+import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 
 import javax.lang.model.element.Element;
@@ -32,6 +33,22 @@ import javax.lang.model.type.TypeMirror;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Handler for the {@link EventBusGreenRobot} annotation.
+ * <ul>
+ * <li>when there is an item annotated with {@link EventBusGreenRobot}, we will
+ * initialize it in the {@code _init} method and assign the default event bus: {@link EventBus#getDefault()}</li>
+ * <li>in addition, when the parent class has
+ *   <ul>
+ *   <li>life-cylce methods (e.g. {@link org.androidannotations.annotations.EActivity},
+ *   {@link org.androidannotations.annotations.EService}, etc.)</li>
+ *    <li>AND at least one {@link Subscribe} annotation</li>
+ * </ul>
+ * we will also add {@link EventBus#register(Object)}/{@link EventBus#unregister(Object)} calls in the
+ * {@code onStart}/{@code onStop} methods
+ * </li>
+ * </ul>
+ */
 public class EventBusGreenRobotHandler
         extends BaseAnnotationHandler<EComponentHolder>
         implements MethodInjectionHandler<EComponentHolder> {
@@ -50,6 +67,10 @@ public class EventBusGreenRobotHandler
             return;
         }
 
+        /* the element in the base class must not be private, because
+         * we must be able to assign the event-bus instance to it
+         * in the child-class
+         */
         validatorHelper.isNotPrivate(element, validation);
 
         List<String> allowedTypes = new ArrayList<>();
@@ -86,18 +107,26 @@ public class EventBusGreenRobotHandler
     }
 
     /**
-     * initializes the EventBusGreenRobot member variable in the init_ method
-     * with the default EventBusGreenRobot
+     * initializes the event-bus member variable
+     * <p>we will add code to the in the {@code init_} method, to initialize
+     * the event-bus member variable (which is annotated with {@link EventBusGreenRobot}
+     * and is of type {@link org.greenrobot.eventbus.EventBus} with the
+     * default event-bus: {@link EventBus#getDefault()}
+     * </p>
+
+     * @param targetBlock  the code block where the initialization code is added to
+     * @param fieldRef  the reference to the event-bus member variable.
+     * @param param  the annotated element
      */
     private void initEventBusMember(JBlock targetBlock, IJAssignmentTarget fieldRef, Element param) {
         TypeMirror fieldType = param.asType();
-        // fieldTypeQualifiedName = "org.greenrobot.greenannotations.EventBusGreenRobot"
+        // fieldTypeQualifiedName = "org.greenrobot.eventbus.EventBus"
         String fieldTypeQualifiedName = fieldType.toString();
 
         AbstractJClass eventBusClass = getJClass(fieldTypeQualifiedName);
-        // expression = org.greenrobot.greenannotations.EventBusGreenRobot.getDefault()
+        // expression = org.greenrobot.eventbus.EventBus.getDefault()
         IJExpression expression = eventBusClass.staticInvoke("getDefault");
-        // statement = this.bus = org.greenrobot.greenannotations.EventBusGreenRobot.getDefault()
+        // statement = this.bus = org.greenrobot.eventbus.EventBus.getDefault()
         IJStatement statement = fieldRef.assign(expression);
         targetBlock.add(statement);
     }
@@ -105,6 +134,12 @@ public class EventBusGreenRobotHandler
     /**
      * returns true when the element has at least one method that uses
      * the @{@link Subscribe} annotation
+     * <p>
+     * then we must register/unregister the class with the EventBus
+     * </p>
+     *
+     * @param element  the element to check for the @{@link Subscribe} annotation
+     * @return if we must register/unregister the EventBus
      */
     private boolean hasSubscribeAnnotation(Element element) {
         boolean hasSubscribeAnnotation = false;
@@ -123,7 +158,10 @@ public class EventBusGreenRobotHandler
     }
 
     /**
-     * adds register/unregister calls for the greenannotations to the onStart/onStop methods
+     * adds register/unregister calls for the EventBus to the onStart/onStop methods
+     *
+     * @param fieldRef  is the field reference ({@code this.bus}) that is annotated the {@link EventBusGreenRobot} annotation
+     * @param holderWithLifecycleMethods  the component holder which implements {@link HasLifecycleMethods}
      */
     private void handleEventBusRegistration(IJAssignmentTarget fieldRef
             , HasLifecycleMethods holderWithLifecycleMethods) {
