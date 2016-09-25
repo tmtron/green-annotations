6e586f699db1ab1a2e1d39e3c74c8c380de2b455
martin.trummer <martin.trummer@tmtron.com>
2016-09-25 13:25:48 +0000

    added more tests

---
+++ green-annotations-test/src/main/java/com/tmtron/greenannotations/test/ActivityBefore.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright © 2016 Martin Trummer (martin.trummer@tmtron.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tmtron.greenannotations.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+/**
+ * test class to show how to use EventBus without GreenAnnotations
+ */
+public class ActivityBefore extends Activity {
+
+    EventBus eventBus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        eventBus = EventBus.getDefault();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        eventBus.register(this);
+    }
+
+    @Override
+    public void onStop() {
+        eventBus.unregister(this);
+        super.onStop();
+    }
+
+    void fireEvent(String message) {
+        eventBus.post(new MessageEvent(message));
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(MessageEvent event) {
+        Toast.makeText(getApplicationContext(), event.message, Toast.LENGTH_SHORT).show();
+    }
+
+}
